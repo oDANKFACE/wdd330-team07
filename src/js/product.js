807100c0e270a81124a3d69e8e76aaa05f17c84c
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-04-27 17:45:40 -0600

    cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,30 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+function addProductToCart(product) 
+{
+  //change to getLocalStorage. setLocalStorage will replace the item with the new one when adding more than one item.
+  let items = getLocalStorage('so-cart') || [];
+
+  if(!Array.isArray(items))
+  {
+    items = [items];
+  }
+  items.push(product);
+  console.log(items);
+
+  //add all items to the cart
+  setLocalStorage('so-cart', items);
 }
+
+
 // add to cart button event handler
-async function addToCartHandler(e) {
+async function addToCartHandler(e) 
+{
   const product = await dataSource.findProductById(e.target.dataset.id);
+  console.log(product);
   addProductToCart(product);
 }
 
