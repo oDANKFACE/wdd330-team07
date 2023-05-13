ddf81690b98093342a1fe336540dc187f63dc737
heidicb1 <131500888+heidicb1@users.noreply.github.com>
2023-05-13 08:17:12 -0600

    fixing the backpack quantity

--- src/js/product_details.mjs
+++ src/js/product_details.mjs
@@ -34,7 +34,7 @@ export default class ProductDetails
   {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
-     
+    updateCartNumber() 
     console.log(this.productId)
     console.log(this.dataSource.findProductById(this.productId))
     // once we have the product details we can render out the HTML
